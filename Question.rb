f77fd70d267f16a729acc0c0eedba24ea5ae10cb
Sara Irving <sarakirving@gmail.com>
2020-08-17 17:57:15 +0000

    create Question class which generates two random integers between 1 and 20 and uses them to formulate a question to ask of the player

---
+++ Question.rb
@@ -0,0 +1,16 @@
+
+
+class Question
+
+  attr_reader :num1, :num2
+
+  def initialize
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
+  end
+
+  def genertateQuestion
+    "What is #{@num1} + #{@num2}?"
+  end
+  
+end
