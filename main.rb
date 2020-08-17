c609094c2ad8435005c3fc46285535c1f754650e
Sara Irving <sarakirving@gmail.com>
2020-08-17 18:12:24 +0000

    tests for Turn class behavior

--- main.rb
+++ main.rb
@@ -1,8 +1,23 @@
 require "./Player"
 require "./Question"
+require "./Turn"
 
+# tests for Player Class
 player1 = Player.new("Player1")
 puts player1.name
+puts player1.lives
+player1.decreaseLives
+puts player1.lives
 
+player2 = Player.new("Player2")
+
+# tests for Question Class
 ques = Question.new
 puts ques.genertateQuestion
+
+# tests for Turn Class
+turn = Turn.new 
+whoTurn = turn.determineWhosTurn(player1.turnStatus, player2.turnStatus)
+puts whoTurn
+
+#tests for Game Class 
