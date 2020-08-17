2d49665d28a278e1228432dbe742166e0c80cd57
Sara Irving <sarakirving@gmail.com>
2020-08-17 20:20:11 +0000

    add inspect method for debugging purposes, remove turn status variable since we did not end up needing to use it

--- Player.rb
+++ Player.rb
@@ -2,18 +2,23 @@
 
 class Player 
   
-  attr_accessor :name, :turnStatus
+  attr_accessor :name
   attr_reader :lives
 
   def initialize(name)
     @name = name
     @lives = 3
-    @turnStatus = false
   end
 
   def decreaseLives
     @lives -= 1
   end
+
+  def inspect
+    "Player - name: #{name}, lives: #{lives}"
+  end
+  
+
 
 
 end
