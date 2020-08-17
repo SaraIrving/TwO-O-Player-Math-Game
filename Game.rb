38b488ca4519ce95824ff7d477031ed33d41ec82
Sara Irving <sarakirving@gmail.com>
2020-08-17 20:19:26 +0000

    refactor checkGameStatus to check each players remaining lives and give appropriate messages

--- Game.rb
+++ Game.rb
@@ -2,8 +2,16 @@
 
 class Game
 
-  def validate
-    return 77
+  def checkGameStatus(p1_lives, p2_lives)
+    if p1_lives > 0 && p2_lives > 0
+      return "P1: #{p1_lives}/3 vs P2: #{p2_lives}/3 \n ----NEW TURN----" 
+    elsif p1_lives == 0
+      return "Player 2 wins with the score of #{p2_lives}/3 \n ----GAME OVER --- \n Good Bye!"
+    elsif p2_lives == 0
+      return "Player 1 wins with the score of #{p1_lives}/3 \n ----GAME OVER --- \n Good Bye!"
+      
+    end
   end
+
 
 end
