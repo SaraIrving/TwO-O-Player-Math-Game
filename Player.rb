1049a1298462da070d93a7998cb72b006c4fb86e
Sara Irving <sarakirving@gmail.com>
2020-08-17 18:12:04 +0000

    add turnStatus instance variable which is a boolean that is true when it is that players turn and false when it is not

--- Player.rb
+++ Player.rb
@@ -2,11 +2,18 @@
 
 class Player 
   
-  attr_accessor :name, :lives 
+  attr_accessor :name, :turnStatus
+  attr_reader :lives
 
   def initialize(name)
     @name = name
     @lives = 3
+    @turnStatus = false
   end
+
+  def decreaseLives
+    @lives -= 1
+  end
+
 
 end
